9dc3ab8a357a6cad1351db1c6d9588263b6e1580
Laurence <laurenceliao123@gmail.com>
2024-01-13 12:34:53 -0800

    Update backend.py

--- couponchef-spontacular-backend/backend.py
+++ couponchef-spontacular-backend/backend.py
@@ -7,7 +7,7 @@ file_path = '/Users/laurenceliao/Documents/GitHub/CouponChef/couponchef-spontacu
 
 
 #spoonacular stuff
-api_key = 'cf15ffd940b4403e82b0a9aeb727b1d8'
+api_key = ''
 end_point = 'https://api.spoonacular.com/recipes/findByIngredients'
 
 # print the contents of json file
